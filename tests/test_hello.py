236d0ec6d642fd6d6bc9390bca4b363427286c11
Dae Woo Kim <daewoo.kim@live.com>
2023-07-11 21:23:23 +0900

    update lint and unit test

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,12 +1,14 @@
 import pytest
 from src.hello import app
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+
 def test_hello_world(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert b"<p>Hello, World!</p>" in response.data
+    assert b"<h1>Hello, World!</h1>" in response.data
